b01ae0188d104309cde8aa3120f602529455770a
benizar <benizar@gmail.com>
2016-11-04 13:07:13 +0100

    removed setup indexes

--- ext/sql/setup/indexes/indexes.sql
+++ ext/sql/setup/indexes/indexes.sql
@@ -1,11 +1,11 @@
 -- Index for give_me_pids
-CREATE INDEX give_me_pids_idx ON dms.docstore (ST_GeoHash(where_centroid));
+--CREATE INDEX give_me_pids_idx ON dms.docstore (ST_GeoHash(where_centroid));
 
 -- Indexes for give_me_pyramids
-CREATE INDEX give_me_pyramids_idx ON dms.docstore (pid);
+--CREATE INDEX give_me_pyramids_idx ON dms.docstore (pid);
 
-CREATE INDEX on dms.docstore USING GIN (payload);
+--CREATE INDEX on dms.docstore USING GIN (payload);
 
 --Cluster based on indexes
-CLUSTER dms.docstore USING give_me_pids_idx;
-CLUSTER dms.docstore USING give_me_pyramids_idx;
+--CLUSTER dms.docstore USING give_me_pids_idx;
+--CLUSTER dms.docstore USING give_me_pyramids_idx;
--- ext/sql/setup/triggers/refresh_materialized_views.sql
+++ ext/sql/setup/triggers/refresh_materialized_views.sql
@@ -5,12 +5,17 @@ create or replace function dms.refresh_mat_views()
 returns trigger language plpgsql
 as $$
 begin
-    refresh materialized view dms.main;
-    refresh materialized view dms.docstore;
-    refresh materialized view dms.stats_general;
-    refresh materialized view dms.ui_general_options;
-    refresh materialized view dms.ui_map_catalog;
-    return null;
+	refresh materialized view dms.face_to_face_inespain;
+	refresh materialized view dms.give_me_five_inespain;
+	refresh materialized view dms.map_catalog_inespain;
+	refresh materialized view dms.one_by_one_inespain;
+
+    --refresh materialized view dms.main;
+    --refresh materialized view dms.docstore;
+    --refresh materialized view dms.stats_general;
+    --refresh materialized view dms.ui_general_options;
+    --refresh materialized view dms.ui_map_catalog;
+    	return null;
 end $$;
 
 
