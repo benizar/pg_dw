5adb217438938d3071afc9ed409c8ff11d1d52fe
benizar <benizar@gmail.com>
2016-11-10 08:56:20 +0100

    compose up with appropiate user data

--- initdb-pg_popyramids.sh
+++ initdb-pg_popyramids.sh
@@ -8,6 +8,12 @@ export PGUSER="$POSTGRES_USER"
 # Create the 'popyramids_db'
 "${psql[@]}" <<- 'EOSQL'
 	CREATE DATABASE popyramids_db;
+
+	-- create a new role that will be used for application SELECT queries only.
+	CREATE ROLE apis NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL '2020-06-02 00:00:00';
+	CREATE ROLE postgrest LOGIN ENCRYPTED PASSWORD 'md5ea4c9e85db3ee26e23d65bb44d7e7733' NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION VALID UNTIL '2020-06-02 00:00:00';
+	GRANT apis TO postgrest;
+
 EOSQL
 
 # Load extensions into both popyramids_db and $POSTGRES_DB databases
@@ -15,7 +21,6 @@ for DB in popyramids_db "$POSTGRES_DB"; do
 	echo "Loading extensions into $DB"
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS postgis;
-		CREATE EXTENSION IF NOT EXISTS postgis_topology;
 		CREATE EXTENSION pg_popyramids;
 EOSQL
 done
